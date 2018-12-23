a5d71ad91241c6ee5273444c0897ea0b31b12cb2
uiui611 <uiui611@gmail.com>
2018-12-23 23:14:38 +0900

    Add test about iterate for two or more trees.

--- src/tree.test.mjs
+++ src/tree.test.mjs
@@ -18,5 +18,22 @@ describe('Check Tree Container', ()=>{
                 [ 'child A', 'child B' ]
             );
         });
-    })
+        it('Iterate twice or more.', ()=>{
+            const root = {
+                name: 'root',
+                children:[
+                    { name: 'child A'},
+                    { name: 'child B'}
+                ]
+            };
+            const foundsList = [];
+            for(let i=0;i<3;i++){
+                const founds = [];
+                for(let node of new Tree(root)) founds.push(node);
+                foundsList.push(founds);
+            }
+            assert.deepStrictEqual(foundsList[1], foundsList[0]);
+            assert.deepStrictEqual(foundsList[2], foundsList[0]);
+        });
+    });
 });
